5ad27d74942b9c3a04de9f3ae8886bff3f2262ef
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2023-07-31 01:10:34 -0700

    Input original details into card

--- src/components/homepage/Card.tsx
+++ src/components/homepage/Card.tsx
@@ -6,6 +6,9 @@ import { AppContextData } from "../../context/AppContext";
 // import Rating from "./Rating";
 interface cardProp {
   children: React.ReactNode;
+  imageUrl: string[];
+  name: string;
+  price: number;
 }
 export const Card: React.FC<cardProp> = (props: cardProp) => {
   const { setAddProduct } = useContext(AppContextData);
@@ -13,33 +16,40 @@ export const Card: React.FC<cardProp> = (props: cardProp) => {
     e.preventDefault();
     setAddProduct!((count) => count + 1);
   };
+
   return (
     <div className="">
       <div className="CARDS">
         <Link
           to="/product/details"
-          className="border w-1/4 p-4 flex flex-col gap-5 shadow-sm rounded hover:shadow-xl m-10"
+          className="border  p-4 flex flex-col gap-2 shadow-sm rounded hover:shadow-xl m-2"
         >
-          <img src="../../public/images/background.jpg" className="" alt="" />
-          <div className="Project_Name font-bold text-xl">
-            Lorem ipsum dolor sit
+          <div className=" flex justify-center ">
+            <img
+              src={`${props.imageUrl[0]}`}
+              className="w-60 h-60 rounded"
+              alt=""
+            />
           </div>
-          <div className="Rating_stars ">*****</div>
-          <div>Price: 500$</div>
-          <div className="flex gap-3 justify-end">
+          <div className="flex flex-col gap-2 items-center">
+            <div className="Medicine_Name font-bold text-xl">{props.name}</div>
+            <div className="Rating_stars "></div>
+            <div className=" text-3xl">{props.price}$</div>
+          </div>
+          <div className="flex gap-3 justify-center">
             <button
               onClick={addToCart}
-              className="px-4 w-2/6 flex justify-center py-2 bg-blue-400 rounded hover:bg-blue-500 transition duration-200 text-white font-bold gap-2 items-center"
+              className="px-4 w-5/6 flex justify-center py-2 bg-blue-400 rounded hover:bg-blue-500 transition duration-200 text-white font-bold gap-2 items-center"
             >
               <FontAwesomeIcon icon={faCartShopping} />
               Add
             </button>
-            <Link
+            {/* <Link
               to="/buy/now"
               className="px-4 py-2 w-2/6 bg-green-400 rounded hover:bg-green-500 transition duration-200 text-white font-bold"
             >
               Buy Now
-            </Link>
+            </Link> */}
           </div>
         </Link>
       </div>
